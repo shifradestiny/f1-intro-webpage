d35527eb0b564bb0aabf3df5d88444030b38d351
Shifra Destiny Ssebuliba <destinyssebuliba@gmail.com>
2025-07-27 16:25:27 +0200

    Created function to handle button interaction

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function calendar() {
+  let name = prompt("What is your name?");
+  let email = prompt("What is your email?");
+  let raceCalendar = prompt("Are you a beginner to F1?");
+
+  raceCalendar = raceCalendar.toLowerCase();
+
+  if (raceCalendar === "no") {
+    alert(
+      "Thank you " +
+        name +
+        "! We will forward a copy of the 2024 F1 Race Calendar to your email shortly. 😊"
+    );
+  } else {
+    alert(
+      "Thank you " +
+        name +
+        "! We will forward a copy of the 2024 F1 Race Calendar, along with a beginners guide to F1, to your email shortly. 😊"
+    );
+  }
+}
